b743df9aa78a2dfd59c03c1529ab2b89991eef3f
Yann Klein <yann.klein@me.com>
2022-01-13 22:26:48 +0900

    livecode done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_13_114529) do
+ActiveRecord::Schema.define(version: 2022_01_13_123711) do
 
   create_table "gardens", force: :cascade do |t|
     t.string "name"
@@ -19,4 +19,14 @@ ActiveRecord::Schema.define(version: 2022_01_13_114529) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "plants", force: :cascade do |t|
+    t.string "name"
+    t.string "image_url"
+    t.integer "garden_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["garden_id"], name: "index_plants_on_garden_id"
+  end
+
+  add_foreign_key "plants", "gardens"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 if Rails.env.development?
   puts "Destroying all the gardens and tags..."
-  # Plant.destroy_all
+  # Plant.destroy_all not needed becaue we have dependent destroy
   Garden.destroy_all
 end
 
@@ -20,29 +20,29 @@ Garden.create!(
   banner_url: "https://upload.wikimedia.org/wikipedia/commons/0/0b/Ritsurin.JPG"
   )
 
-# puts "Seed plants..."
-# Plant.create!(
-#   name: "Bonzai",
-#   image_url: "http://iris-bleu.i.r.pic.centerblog.net/o/2632ebdc.jpg",
-#   garden: shinjuku
-# )
+puts "Seed plants..."
+Plant.create!(
+  name: "Bonzai",
+  image_url: "http://iris-bleu.i.r.pic.centerblog.net/o/2632ebdc.jpg",
+  garden: shinjuku
+)
 
-# Plant.create!(
-#   name: "Momiji",
-#   image_url: "https://www.thetreecenter.com/wp-content/uploads/japanese-maple-osakazuki-2.jpg",
-#   garden: shinjuku
-# )
+Plant.create!(
+  name: "Momiji",
+  image_url: "https://www.thetreecenter.com/wp-content/uploads/japanese-maple-osakazuki-2.jpg",
+  garden: shinjuku
+)
 
-# Plant.create!(
-#   name: "Sakura tree",
-#   image_url: "https://bitrebels.com/wp-content/uploads/2012/05/cherry-blossom-cupcake-painting-4.jpg",
-#   garden: shinjuku
-# )
+Plant.create!(
+  name: "Sakura tree",
+  image_url: "https://bitrebels.com/wp-content/uploads/2012/05/cherry-blossom-cupcake-painting-4.jpg",
+  garden: shinjuku
+)
 
-# Plant.create!(
-#   name: "Baobab",
-#   image_url: "https://s3.amazonaws.com/cdn.matadornetwork.com/blogs/1/2018/06/Baobab-tree-1200x853.jpg",
-#   garden: shinjuku
-# )
+Plant.create!(
+  name: "Baobab",
+  image_url: "https://s3.amazonaws.com/cdn.matadornetwork.com/blogs/1/2018/06/Baobab-tree-1200x853.jpg",
+  garden: shinjuku
+)
 
 puts "Seeding done!"
